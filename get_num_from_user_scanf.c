481c1fec0415251a84a29bd60cfdbb62a6bbe65e
sanjeev <sanjeevsigamani@gmail.com>
2021-10-25 09:39:50 -0400

    C files are here

---
+++ get_num_from_user_scanf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{   
+    int num;
+    printf("enter a number");
+    scanf("%d",&num);
+
+    printf("now i am letter you on a secret...\n");
+    printf("you have just entered the number %d\n",num);
+    return 0;
+
+ }
